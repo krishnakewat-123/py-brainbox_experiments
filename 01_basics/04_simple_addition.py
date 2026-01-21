04aaa6de188a07e7df61ba176e9284c33c516c49
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-21 15:52:20 +0530

    Add simple addition program in Python

---
+++ 01_basics/04_simple_addition.py
@@ -0,0 +1,17 @@
+'''####### output #######
+Enter first number: 2
+Enter second number: 3
+The sum of 2 & 3 is: 5
+####### end ############
+'''
+# This program adds two numbers entered by the user
+
+# Taking input
+num1 = int (input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+#logic 
+sum = num1 + num2
+
+#printing sum
+print("The sum of", num1, "&", num2, "is:", sum)
