b706efbedc9d15bb91d9f25641bb76d5470b798b
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-21 15:43:36 +0530

    Add variables and data types demonstration program

---
+++ 01_basics/02_variables_data_types.py
@@ -0,0 +1,36 @@
+'''########### output #######################
+Name: Krishna
+Age: 25
+Height: 5.6
+Is Student: True
+
+Data Types:
+Type of name: <class 'str'>
+Type of age: <class 'int'>
+Type of height: <class 'float'>
+Type of is_student: <class 'bool'>
+###########################################
+'''
+# Program 2: Variables and Data Types
+# This program demonstrates variables of different types
+# String variable
+name = "Krishna"
+# Integer variable
+age = 21
+# Float variable
+height = 5.6
+# Boolean variable
+is_student = True
+
+# Printing values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Print the type of each variable
+print("\nData Types:")
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
