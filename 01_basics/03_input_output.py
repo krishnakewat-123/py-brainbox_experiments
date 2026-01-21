0b983ed76100c6d47c355811b48cd703eb2a44c3
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-21 15:48:15 +0530

    Add user input and output functionality
    This program takes user input for name and age, then displays a greeting and age information.

---
+++ 01_basics/03_input_output.py
@@ -0,0 +1,16 @@
+'''####### output #######
+Enter your name: krishna
+Enter your age: 21
+@Hello, krishna!
+You are 21 years old.
+####### code ############
+'''
+# This program takes input from the user and displays it
+
+# Taking input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Printing 
+print("@Hello,", name + "!")
+print("You are", age, "years old.")
