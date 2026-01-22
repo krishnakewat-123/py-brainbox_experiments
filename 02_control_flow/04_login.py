bdc0a0356a49d624be97c4064da6b1a41d809742
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-22 13:21:55 +0530

    Add login functionality with username and password

---
+++ 02_control_flow/04_login.py
@@ -0,0 +1,12 @@
+'''######### output ######
+Enter username: krishna
+Enter password: 1234
+Login Successful
+####### end ##############'''
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "krishna" and password == "1234":
+    print("Login Successful")
+else:
+    print("Login Failed")
