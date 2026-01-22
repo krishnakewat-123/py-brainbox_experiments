93ec3dc1697478a2cd2c05f9ff3a2be5cc0fc0af
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-22 14:44:56 +0530

    Add while loop for printing numbers 1 to 5
    Implement a while loop to print numbers from 1 to 5 in a single line.

---
+++ 03_loops/02_while_loop/01_while(loop)_Initialization.py
@@ -0,0 +1,10 @@
+'''######### output ######
+      1,2,3,4,5,
+######## end #############
+'''
+i = 1        # initialization
+
+while i <= 5:   # condition
+    print(i,end=",") for single line
+    i += 1 # update
+       
