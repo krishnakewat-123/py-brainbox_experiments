23b6fba50bfb76df9b653a1f5431e0bda7f84bfb
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-30 15:33:05 +0530

    Implement sum of n numbers using a while loop
    Fix the while loop increment to correctly sum numbers.

---
+++ 03_loops/02_while_loop/02_sum_of_n_numbers.py
@@ -0,0 +1,14 @@
+'''########### output######
+Enter a number: 4
+Sum is: 10
+###########################
+'''
+n =int(input("Enter a number: "))
+sum =0
+i =1
+
+while i <= n:
+    sum += i
+    i += 1
+
+print("Sum is:", sum)
