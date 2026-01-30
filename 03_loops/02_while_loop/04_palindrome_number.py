66a0d9d292f2e9723d267c31dd5b5de6ea1ab525
krishna kumar kewat <159262509+krishnakewat-123@users.noreply.github.com>
2026-01-30 15:58:11 +0530

    Add palindrome number checker in Python

---
+++ 03_loops/02_while_loop/04_palindrome_number.py
@@ -0,0 +1,17 @@
+'''######## output ########
+Enter a number: 121
+Palindrome number
+######## output ########'''
+
+num = int(input("Enter a number: "))
+temp = num
+reverse = 0
+
+while temp > 0:
+    reverse = reverse * 10 + temp % 10
+    temp //= 10
+
+if num == reverse:
+    print("Palindrome number")
+else:
+    print("Not a palindrome")
